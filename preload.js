600d2ff51d6600b23958e4534d4c038ad003d10d
Agornyrah Eric <eagornyrah@quantumgroupgh.com>
2026-09-08 11:21:17 -0700

    build: add a one-way ipc communication from main to renderer - part 3

--- preload.js
+++ preload.js
@@ -2,7 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
 
-  // EXPLAIN: 1. expose the ipcRenderer to the electronAPI to be listened by an ipcMain API
-  openFile: () => ipcRenderer.invoke('dialog:openFile')
+  // EXPLAIN: 1. expose the ipcRenderer to the electronAPI to be recorded by an ipcMain API
+  onUpdateCounter: (callback) => ipcRenderer.on('update-counter', (_event, value) => callback(value)),
+  counterValue: (value) => ipcRenderer.send('counter-value', value)
 
 })
