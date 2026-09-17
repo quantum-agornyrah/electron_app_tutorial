040ec64ce6a997b17d0b5494cc7552a6adc90928
Agornyrah Eric <eagornyrah@quantumgroupgh.com>
2026-09-17 15:42:40 -0700

    build: add tray menus

--- main.js
+++ main.js
@@ -1,46 +1,60 @@
-const { app, BrowserWindow, Menu } = require('electron/main')
-const { shell } = require('electron/common')
+const { app, BrowserWindow, Menu, Tray } = require('electron/main')
+const { nativeImage } = require('electron/common')
+
+// save a reference to the Tray object globally to avoid garbage collection
+let tray = null
 
 function createWindow () {
-  const win = new BrowserWindow()
-  win.loadFile('index.html')
+  const mainWindow = new BrowserWindow()
+  mainWindow.loadFile('index.html')
 }
 
-function closeAllWindows () {
-  const wins = BrowserWindow.getAllWindows()
-  for (const win of wins) {
-    win.close()
-  }
-}
-
+// The Tray object can only be instantiated after the 'ready' event is fired
 app.whenReady().then(() => {
   createWindow()
 
-  const dockMenu = Menu.buildFromTemplate([
+  const red = nativeImage.createFromDataURL('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAACTSURBVHgBpZKBCYAgEEV/TeAIjuIIbdQIuUGt0CS1gW1iZ2jIVaTnhw+Cvs8/OYDJA4Y8kR3ZR2/kmazxJbpUEfQ/Dm/UG7wVwHkjlQdMFfDdJMFaACebnjJGyDWgcnZu1/lrCrl6NCoEHJBrDwEr5NrT6ko/UV8xdLAC2N49mlc5CylpYh8wCwqrvbBGLoKGvz8Bfq0QPWEUo/EAAAAASUVORK5CYII=')
+  const green = nativeImage.createFromDataURL('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAACOSURBVHgBpZLRDYAgEEOrEzgCozCCGzkCbKArOIlugJvgoRAUNcLRpvGH19TkgFQWkqIohhK8UEaKwKcsOg/+WR1vX+AlA74u6q4FqgCOSzwsGHCwbKliAF89Cv89tWmOT4VaVMoVbOBrdQUz+FrD6XItzh4LzYB1HFJ9yrEkZ4l+wvcid9pTssh4UKbPd+4vED2Nd54iAAAAAElFTkSuQmCC')
+
+  tray = new Tray(red)
+  tray.setToolTip('Tray Icon Demo')
+
+  const contextMenu = Menu.buildFromTemplate([
     {
-      label: 'New Window',
-      click: () => { createWindow() }
-    },
-    {
-      label: 'Close All Windows',
-      click: () => { closeAllWindows() }
-    },
-    {
-      label: 'Open Electron Docs',
+      label: 'Open App',
       click: () => {
-        shell.openExternal('https://electronjs.org/docs')
+        const wins = BrowserWindow.getAllWindows()
+        if (wins.length === 0) {
+          createWindow()
+        } else {
+          wins[0].focus()
+        }
       }
-    }
-    // add more menu options to the array
+    },
+    {
+      label: 'Set Green Icon',
+      type: 'checkbox',
+      click: ({ checked }) => {
+        checked ? tray.setImage(green) : tray.setImage(red)
+      }
+    },
+    {
+      label: 'Set Title',
+      type: 'checkbox',
+      click: ({ checked }) => {
+        checked ? tray.setTitle('Title') : tray.setTitle('')
+      }
+    },
+    { role: 'quit' }
   ])
 
-  app.dock.setMenu(dockMenu)
-
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+  tray.setContextMenu(contextMenu)
 })
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  // This will prevent the app from closing when windows close
+})
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
